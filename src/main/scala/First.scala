aaa8a8cc756fbf3be36d8c005466afb59b4a3fb9
tong.yue <tong.yue@hypers.com>
2016-07-13 18:07:06 +0800

    spark

--- src/main/scala/First.scala
+++ src/main/scala/First.scala
@@ -1,10 +1,23 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Tong on 2016/7/13.
+  *
+  * /user/t_test/ATTRIBUTION
   */
 object First{
+
   def main(args: Array[String]) {
 
-    print(123)
+    val conf=new SparkConf().setAppName("testConversionModel")
+
+    val sc=new SparkContext(conf)
+
+    val hadoopFile = sc.hadoopFile("/user/t_test/ATTRIBUTION/20160624/")
+
+
 
   }
+
 }
